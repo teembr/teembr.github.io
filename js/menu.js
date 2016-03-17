b96539acaa8cb6c47779d21c65002d0734fbf509
Daisyrie Dumali <daisyriedumali@Daisyries-MacBook-Pro.local>
2016-03-17 22:00:51 +0800

    fix forms and scrolls

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     $(".gotohere").on("click", function(e) {
         e.preventDefault();
 
-        $("body, html").animate({ scrollTop: $( $(this).attr('href') ).offset().top - 90 }, 600);
-
+        $("body, html").animate({ scrollTop: $( $(this).attr('href') ).offset().top - 70 }, 600);     
     });
 });
