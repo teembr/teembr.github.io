7499efba246c51edfddc92d8dfa439b01da82c5a
Daisyrie Dumali <daisyriedumali@Daisyries-MacBook-Pro.local>
2016-05-17 23:41:21 +0800

    workhover.js forgot to commit

---
+++ js/workhover.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $('.img-zoom').hover(function() {
+        $(this).addClass('transition');
+ 
+    }, function() {
+        $(this).removeClass('transition');
+    });
+
+});
