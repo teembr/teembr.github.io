e4afb4cb97a0a1741f3c3c8c3d1100d50761c0c6
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2016-10-07 22:55:38 +0800

    teembr v2 redesign version

---
+++ js/granim.js
@@ -0,0 +1,16 @@
+var granimInstance = new Granim({
+    element: '.canvas-img',
+    direction: 'left-right',
+    opacity: [.9, .9, .9],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#00c6ff', '#0072ff', '#0072ff'],
+                ['#eb3fd9', '#cc00b7', '#eb3fd9'],
+                ['#ffb20a', '#ff9106', '#ffb20a']
+            ],
+            transitionSpeed: 3800
+        }
+    }
+});
