d5b0d21465b5c6def2b786537d29a4fc1c07db5e
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2016-11-19 20:27:20 +0800

    updated page and app id

--- js/messengerBtn.js
+++ js/messengerBtn.js
@@ -1,6 +1,5 @@
 $(window).scroll(function() {
    if( ($(window).scrollTop() + $(window).height()) > ($(document).height() - 100) ) {
-        console.log('asd');
         $(".fb-messenger").css("display", "none");
    }
    else {
